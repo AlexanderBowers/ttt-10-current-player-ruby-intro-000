4571beefd56be178b34c65535bedff68c25433f2
Alexander Bowers <7319abowers@gmail.com>
2020-09-26 20:06:35 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,8 +1,8 @@
 def turn_count(board, index)
-board.each do |count|
-  if board[index] == "X" || board[index] == "O"
+board.each do |count|  #look at each space of board array and creates a variable called count
+  if board[index] == "X" || board[index] == "O" #look at each index to see if X or O
     do
-      count +=1
+      count +=1 #If it contains X or O, increase the count by 1.
     end
   end
 end
