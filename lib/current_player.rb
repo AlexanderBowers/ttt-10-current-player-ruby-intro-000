4c2911d5aa228914c9d7ed428f0f223142cd566d
Alexander Bowers <7319abowers@gmail.com>
2020-09-26 22:05:27 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,20 +1,4 @@
-require 'pry'
-def turn_count(board)
-  counter = 0
-  board.each do |count|  #look at each space of board array and creates a variable called count
-    if count = position_taken?(board, count) #while going through each index, verify if it's taken.
-      counter += 1 #if position is taken, add 1 to counter.
-    end
-  end
-  binding.pry
-  puts "There are #{counter} spaces taken." #after going through all spaces of index, print how many spaces are taken.
-end
 
-
-def position_taken? (board, index)
-  taken = board[index] == "X" || board[index] == "O"
-return taken
-end
 
 def current_player(board)
 end
