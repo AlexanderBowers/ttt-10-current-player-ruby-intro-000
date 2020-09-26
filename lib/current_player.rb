52576a9c2ee0e375ac3349451080b571adfdd5b6
Alexander Bowers <7319abowers@gmail.com>
2020-09-26 22:16:50 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,6 +1,6 @@
 def turn_count(board)
   counter = 0 #create a variable callec counter
-  board.each do |count|
+  board.each do |count| #create a variable called count that goes through each index of the board array
     board[count] = position_taken?(board, count)
     counter +=1
   end
