f391ae650299cccb33f5083895ac7ec0cfc4261c
Alexander Bowers <7319abowers@gmail.com>
2020-09-26 20:10:09 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -5,3 +5,7 @@ board.each do |count|  #look at each space of board array and creates a variable
     end
   end
 end
+
+
+def position_taken? (board, index)
+  taken = board[index] == "X" || board[index] == "O"
