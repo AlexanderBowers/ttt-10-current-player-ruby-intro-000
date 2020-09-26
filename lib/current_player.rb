1e832daa6697d9ec4319b915dd13a30c4464e562
Alexander Bowers <7319abowers@gmail.com>
2020-09-26 20:02:43 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,2 @@
-def turn_count(turn(board, index))
-  counter = 0
-  turn(board, index).each do |turn_count|
-    puts "This is turn #{turn_count}"
-    counter += 1
-  end
+def turn_count(turn)
 end
