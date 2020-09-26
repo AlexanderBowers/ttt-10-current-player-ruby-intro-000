3d28745cf7f1e0356eaf76b7ae356e8cc27856a1
Alexander Bowers <7319abowers@gmail.com>
2020-09-26 22:20:31 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -3,7 +3,7 @@ def turn_count(board)
   board.each do |count| #create a variable called count that goes through each index of the board array
     if board[count] == "X" || board[count] == "O" #check if the index is occupied by X or O
     counter +=1 #if the space has X or O, add 1 to counter.
-  end
+    end
   end
   return counter
 end
