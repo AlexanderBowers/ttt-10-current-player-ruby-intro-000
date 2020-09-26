df447f3842eefa1a309bb5a97484fd00968d939b
Alexander Bowers <7319abowers@gmail.com>
2020-09-26 20:08:20 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,8 +1,11 @@
 def turn_count(board, index)
 board.each do |count|  #look at each space of board array and creates a variable called count
-  if board[index] == "X" || board[index] == "O" #look at each index to see if X or O
+  if board[index] = position_taken? #look at each index to see if X or O
     do
       count +=1 #If it contains X or O, increase the count by 1.
     end
   end
 end
+
+
+#Could use an existing position_taken? method, but 
