3fcda05a63827ef4135a78fb2aeb8f21169658f5
Alexander Bowers <7319abowers@gmail.com>
2020-09-26 21:49:26 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,9 @@
 def turn_count(board, index)
-  board.each do |count|  #look at each space of board array and creates a variable called count
-    if board[index] = position_taken?(board, index)
-      puts "There are #{count} spaces taken"
+  counter = 0
+  board.each do |spaces|  #look at each space of board array and creates a variable called count
+    if board[index] = position_taken?(board, index) #while going through each index, verify if it's taken.
+      counter += 1 #if position is taken, add 1 to counter.
+      puts "There are #{counter} spaces taken"
     end
   end
 end
